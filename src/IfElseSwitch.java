772a1b3f60d44214735e2c8452b2d682f07f9210
rominalodolo <lodoloromina@gmail.com>
2022-04-19 10:25:03 +0200

    update

---
+++ src/IfElseSwitch.java
@@ -0,0 +1,32 @@
+
+public class IfElseSwitch {
+    public static void main(String[] args) {
+        // Finding the largest of two numbers
+        int x;
+        x = 1;
+        if(x==1){
+            System.out.println("One");
+        }else if(x==2) {
+            System.out.println("Two");
+        }else{
+            System.out.println("I don't know the number.");
+        }
+
+        System.out.println("I am learning Switch.");
+        switch(x){
+            case 1:
+                System.out.println("One");
+                break;
+            case 2:
+                System.out.println("Two");
+                break;
+            case 3:
+                System.out.println("I don't know the number.");
+        }
+
+        int num1, num2;
+        if 
+
+
+    }
+}
