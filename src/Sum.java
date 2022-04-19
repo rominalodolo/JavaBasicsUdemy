4e380ee63c222f0c6174daa07c4bb88d257a83df
rominalodolo <lodoloromina@gmail.com>
2022-04-19 09:30:29 +0200

    update

--- src/Sum.java
+++ src/Sum.java
@@ -1,2 +1,13 @@
 public class Sum {
+    public static void main(String[] args) {
+        // finding the sum of two numbers
+        int num1, num2, sum;
+        num1 = 10;
+        num2 = 16;
+        sum = num1 + num2;
+
+        System.out.println("The sum of the two numbers is " + sum);
+
+
+    }
 }
