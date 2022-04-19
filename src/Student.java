caef8cffeecee3ddc028b9f29de3c18cb0828e5a
rominalodolo <lodoloromina@gmail.com>
2022-04-19 11:04:20 +0200

    update

--- src/Student.java
+++ src/Student.java
@@ -1,2 +1,19 @@
 public class Student {
+    String StudentName;
+    int age;
+    int marksMath;
+    int marksEng;
+    int marksArt;
+
+    public void CalculateMarks(){
+        marksMath = 10;
+        marksEng = 20;
+        marksArt = 20;
+        System.out.println("The total marks are " + (marksArt + marksMath + marksEng));
+    }
+
+    public static void main(String[] args) {
+        Student S1 = new Student();
+        S1.CalculateMarks();
+    }
 }
